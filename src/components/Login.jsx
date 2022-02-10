12706f00277c3ba514bf5979a6dd3d742f9c820f
Dylan Gorman <dylangorman@me.com>
2022-02-10 17:25:20 +0000

    css and login button fix

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -39,87 +39,74 @@ function Login({ user, setUser }) {
   }
 
   return (
-    <div className="login-container">
-      <div className="logowrapper">
-        <ul className="circle">
-          <li className="segment green" />
-          <li className="segment darkBlue " />
-          <li className="segment pink" />
-          <li className="segment blue hidden" />
-          <li className="segment purple hidden" />
-          <li className="segment white hidden" />
-          <li className="segment beige hidden" />
-          <li className="segment gray hidden" />
-          <li className="segment black hidden" />
-          <li className="segment red" />
-          <li className="segment orange" />
-          <li className="segment yellow" />
-        </ul>
-      </div>
-      <div className="welcome-info">
-        <h2>Welcome to </h2>
+    <>
+      <div className="login-container">
+        <div className="welcome-info">
+          <h2>Welcome to </h2>
+          <img
+            className="rainbowlogo"
+            src={Rainbow}
+            alt="moodtone logo rainbow colours in a semi circle shape"
+          />
+          <h1>moodTone </h1>
+          <p>
+            The brand new music app that takes care of your emotions! Login here
+            to experience our unique musiSphere and let your emotions fly! If
+            you are new to this App then click on the Register link!
+          </p>
+        </div>
+        <div className="login-form">
+          <div className="form-header">
+            <h1>Login Here..</h1>
+          </div>
+          <form onSubmit={handleSubmit}>
+            <div className="email">
+              <label htmlFor="user">Email:</label>
+              <br />
+              <input
+                type="text"
+                name="user"
+                value={userName}
+                placeholder="email"
+                onChange={handleUserName}
+              />
+            </div>
+            <br />
+
+            <div className="password">
+              <label htmlFor="password">Password:</label>
+              <br />
+              <input
+                type="password"
+                name="password"
+                placeholder="password"
+                value={password}
+                onChange={handlePassword}
+              />
+            </div>
+
+            <div className="footer">
+              <div className="submit-button">
+                <button type="submit" className="btn-sub">
+                  Submit
+                </button>
+              </div>
+              <div className="switch-btn">
+                <Link to="/register" className="btn btn-link">
+                  Register
+                </Link>
+              </div>
+            </div>
+          </form>
+        </div>
+
         <img
-          className="rainbowlogo"
-          src={Rainbow}
-          alt="moodtone logo rainbow colours in a semi circle shape"
+          className="lovebody"
+          src={LoveBody}
+          alt="illustration of girl in pink listening to music on headphones"
         />
-        <h1>moodTone </h1>
-        <p>
-          The brand new music app that takes care of your emotions! Login here
-          to experience our unique musiSphere and let your emotions fly! If you
-          are new to this App then click on the Register link!
-        </p>
       </div>
-      <div className="login-form">
-        <div className="form-header">
-          <h1>Login Here..</h1>
-        </div>
-        <form onSubmit={handleSubmit}>
-          <div className="email">
-            <label htmlFor="user">Email:</label>
-            <br />
-            <input
-              type="text"
-              name="user"
-              value={userName}
-              placeholder="email"
-              onChange={handleUserName}
-            />
-          </div>
-          <br />
-
-          <div className="password">
-            <label htmlFor="password">Password:</label>
-            <br />
-            <input
-              type="password"
-              name="password"
-              placeholder="password"
-              value={password}
-              onChange={handlePassword}
-            />
-          </div>
-        </form>
-        <div className="footer">
-          <div className="submit-button">
-            <button type="submit" className="btn-sub">
-              Submit
-            </button>
-          </div>
-          <div className="switch-btn">
-            <Link to="/register" className="btn btn-link">
-              Register
-            </Link>
-          </div>
-        </div>
-      </div>
-
-      <img
-        className="lovebody"
-        src={LoveBody}
-        alt="illustration of girl in pink listening to music on headphones"
-      />
-    </div>
+    </>
   );
 }
 
