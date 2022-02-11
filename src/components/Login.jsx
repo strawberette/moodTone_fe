21e9d8fe1ff8093aa928a5a9441d0bbe54d80c58
Dylan Gorman <dylangorman@me.com>
2022-02-11 10:26:17 +0000

    splash screen and login finished

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -70,6 +70,7 @@ function Login({ user, setUser }) {
               <label htmlFor="user">Email:</label>
               <br />
               <input
+                className="form-input"
                 type="text"
                 name="user"
                 value={userName}
@@ -83,6 +84,7 @@ function Login({ user, setUser }) {
               <label htmlFor="password">Password:</label>
               <br />
               <input
+                className="form-input"
                 type="password"
                 name="password"
                 placeholder="password"
